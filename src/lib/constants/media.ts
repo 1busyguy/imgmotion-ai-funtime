4902ed876cd461865c58643be36e7e683629549c
James DiMeo <807880+1busyguy@users.noreply.github.com>
2025-06-16 15:01:04 -0400

    Update media.ts

--- src/lib/constants/media.ts
+++ src/lib/constants/media.ts
@@ -4,7 +4,7 @@ export const DEPLOYMENT_IDS = {
     image: '8f96cb86-5cbb-4ad0-9837-8a79eeb5103a',
     video: 'd07cf1d5-412c-4270-b925-ffd6416abd1c',
     firstLastFrameVideo: '8c463102-0525-4cf1-8535-731fee0f93b4',
-    image2video: '87c2a370-0528-413d-b57c-d8afe02a2acc',
+    image2video: 'cbdfe2e3-2cee-4919-ad26-d62a1756ec64',
   } as const;
   
   export const CREDIT_COSTS = {
